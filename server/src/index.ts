448a106dcd95ece0ca5b651c4acf5c9aa76a2c1a
Nov <nov@Novs-MacBook-Air.local>
2026-03-02 23:23:28 +0530

    cors ficex

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,6 +26,7 @@ app.use(
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:5175",
+            "https://bln-k.vercel.app",
             process.env.CLIENT_URL || "http://localhost:5173"
         ],
         credentials: true,
